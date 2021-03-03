5193e0a424a395d490d5d72ebbe7a229de593874
Dan Hayworth <dan.hayworth@hotmail.com>
2021-03-03 23:29:53 +1300

    Add project files.

---
+++ Form1.Designer.cs
@@ -0,0 +1,116 @@
+﻿
+namespace Project
+{
+    partial class frmMain
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(frmMain));
+            this.pFooter = new System.Windows.Forms.Panel();
+            this.aFooter = new System.Windows.Forms.LinkLabel();
+            this.pHeader = new System.Windows.Forms.Panel();
+            this.imgClose = new System.Windows.Forms.PictureBox();
+            this.pFooter.SuspendLayout();
+            this.pHeader.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.imgClose)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pFooter
+            // 
+            this.pFooter.BackColor = System.Drawing.Color.Teal;
+            this.pFooter.Controls.Add(this.aFooter);
+            this.pFooter.Dock = System.Windows.Forms.DockStyle.Bottom;
+            this.pFooter.Location = new System.Drawing.Point(0, 968);
+            this.pFooter.Name = "pFooter";
+            this.pFooter.Size = new System.Drawing.Size(1418, 48);
+            this.pFooter.TabIndex = 0;
+            // 
+            // aFooter
+            // 
+            this.aFooter.ActiveLinkColor = System.Drawing.Color.Maroon;
+            this.aFooter.AutoSize = true;
+            this.aFooter.Font = new System.Drawing.Font("Adobe Caslon Pro", 12F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.aFooter.LinkColor = System.Drawing.Color.Snow;
+            this.aFooter.Location = new System.Drawing.Point(612, 9);
+            this.aFooter.Name = "aFooter";
+            this.aFooter.Size = new System.Drawing.Size(298, 43);
+            this.aFooter.TabIndex = 1;
+            this.aFooter.TabStop = true;
+            this.aFooter.Text = "Developed by Dan Hayworth";
+            this.aFooter.TextAlign = System.Drawing.ContentAlignment.TopCenter;
+            this.aFooter.LinkClicked += new System.Windows.Forms.LinkLabelLinkClickedEventHandler(this.aFooter_LinkClicked);
+            // 
+            // pHeader
+            // 
+            this.pHeader.BackColor = System.Drawing.Color.Teal;
+            this.pHeader.Controls.Add(this.imgClose);
+            this.pHeader.Dock = System.Windows.Forms.DockStyle.Top;
+            this.pHeader.Location = new System.Drawing.Point(0, 0);
+            this.pHeader.Name = "pHeader";
+            this.pHeader.Size = new System.Drawing.Size(1418, 102);
+            this.pHeader.TabIndex = 1;
+            // 
+            // imgClose
+            // 
+            this.imgClose.Image = ((System.Drawing.Image)(resources.GetObject("imgClose.Image")));
+            this.imgClose.Location = new System.Drawing.Point(1359, 27);
+            this.imgClose.Name = "imgClose";
+            this.imgClose.Size = new System.Drawing.Size(47, 46);
+            this.imgClose.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.imgClose.TabIndex = 0;
+            this.imgClose.TabStop = false;
+            this.imgClose.Click += new System.EventHandler(this.imgClose_Click);
+            // 
+            // frmMain
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.AliceBlue;
+            this.ClientSize = new System.Drawing.Size(1418, 1016);
+            this.Controls.Add(this.pHeader);
+            this.Controls.Add(this.pFooter);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Name = "frmMain";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "GUI";
+            this.pFooter.ResumeLayout(false);
+            this.pFooter.PerformLayout();
+            this.pHeader.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.imgClose)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel pFooter;
+        private System.Windows.Forms.LinkLabel aFooter;
+        private System.Windows.Forms.Panel pHeader;
+        private System.Windows.Forms.PictureBox imgClose;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Project
+{
+    public partial class frmMain : Form
+    {
+        public frmMain()
+        {
+            InitializeComponent();
+        }
+
+        private void aFooter_LinkClicked(object sender, LinkLabelLinkClickedEventArgs e)
+        {
+            System.Diagnostics.Process.Start("https://developit.co.nz");
+        }
+
+        private void imgClose_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+    }
+}
